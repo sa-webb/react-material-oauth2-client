bcd692a4cc0903443a2e1337952a6f6bafa2f3eb
sa-webb <sawebb30@gmail.com>
2020-02-12 22:38:42 -0600

    v4 style

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import withRoot from "../theme/withRoot";
+import Header from '../layout/Header';
+
+const App = () => {
+  return (
+    <Header />
+  )
+};
+
+export default withRoot(App);
