5df295a505d95951d9a1e456868e2f2705963498
sa-webb <sawebb30@gmail.com>
2020-02-12 22:38:22 -0600

    update v4 style

---
+++ src/auth/Login.js
@@ -0,0 +1,62 @@
+import React, { useContext } from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+import Context from '../middleware/context';
+
+import { ME_QUERY } from '../graphql/queries.js';
+
+const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+  const onSuccess = async googleUser => {
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient('http://localhost:4000/graphql', {
+        headers: { authorization: idToken }
+      });
+      const { me } = await client.request(ME_QUERY);
+      dispatch({ type: 'LOGIN_USER', payload: me });
+      dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() });
+    } catch (err) {
+      onFailure();
+    }
+  };
+  const onFailure = err => {
+    console.error('Error logging in ', err);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        component='h1'
+        variant='h3'
+        gutterBottom
+        noWrap
+        style={{ color: 'rgb(66, 133, 244)' }}
+      >
+        Welcome
+      </Typography>
+      <GoogleLogin
+        clientId='1076782752033-2knkv8b9tk47s5ddobsv8rrdekt4v4fq.apps.googleusercontent.com'
+        onSuccess={onSuccess}
+        isSignedIn={true}
+        onFailure={onFailure}
+        theme='dark'
+      />
+    </div>
+  );
+};
+
+const styles = {
+  root: {
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+};
+
+export default withStyles(styles)(Login);
---
+++ src/layout/Header.js
@@ -0,0 +1,82 @@
+import React, { useContext } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MapIcon from "@material-ui/icons/Map";
+import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import Context from '../middleware/context';
+import Signout from '../auth/Logout';
+
+const Header = ({ classes }) => {
+  const { state } = useContext(Context);
+  const { currentUser } = state;
+  const mobileSize = useMediaQuery("(max-width: 650px)");
+  return (
+  <div className={classes.root}>
+    <AppBar position="static">
+        <Toolbar>
+          <div className={classes.grow}>
+            <MapIcon className={classes.icon} />
+            <Typography
+              className={mobileSize ? classes.mobile : ""}
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              GeoPins
+            </Typography>
+          </div>
+
+          {currentUser && (
+            <div className={classes.grow}>
+              <img
+                className={classes.picture}
+                src={currentUser.picture}
+                alt={currentUser.name}
+              />
+              <Typography
+                className={mobileSize ? classes.mobile : ""}
+                variant="h5"
+                color="inherit"
+                noWrap
+              >
+                {currentUser.name}
+              </Typography>
+            </div>
+          )}
+
+          <Signout />
+        </Toolbar>
+      </AppBar>
+  </div>
+  )
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1,
+    display: "flex",
+    alignItems: "center"
+  },
+  icon: {
+    marginRight: theme.spacing(0),
+    color: "green",
+    fontSize: 45
+  },
+  mobile: {
+    display: "none"
+  },
+  picture: {
+    height: "50px",
+    borderRadius: "90%",
+    marginRight: theme.spacing(2)
+  }
+});
+
+export default withStyles(styles)(Header);
